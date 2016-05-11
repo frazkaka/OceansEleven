9f09ec537d3a2f296508b1967c52b06cb0e31885
frazkaka <mickefransson1@gmail.com>
2016-05-11 08:55:27 +0200

    yes

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
 <html>
   <head>
+    <title>o-sch-i-en</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/csssheet.css">
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/javascript/javascript.js"></script>
   </head>
   <body>
     <div>
